5725d2c51258d388309ff25caa7b2e81e119af10
Jaison7733 <155734314+Jaison7733@users.noreply.github.com>
2024-12-29 17:41:37 +0000

    123

--- Major_Project/Major_Project.py
+++ Major_Project/Major_Project.py
@@ -44,9 +44,9 @@ df = pd.DataFrame([user_input])
 
 df.replace({'self_employed':{'Yes':1,'No':0},'education':{'Graduate':1,'Not Graduate':0}},inplace=True)
 #df = df.astype(np.int64)
+Major_Project/rfclassifier.pkl
 
-
-rf_classifier = pickle.load(open('https://drive.google.com/file/d/1E9Pk8n7aH7QM--6oLfJchLZVYKaGx-0i/view?usp=drive_link', 'rb'))
+rf_classifier = pickle.load(open('Major_Project/rfclassifier.pkl', 'rb'))
 
 #Major_Project/Model/rfclassifier.pkl
 
